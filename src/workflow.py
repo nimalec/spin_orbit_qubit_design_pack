3f0c6f276a4a9d576da6cefc3492881b656718ff
nimlaec <47722154+nimalec@users.noreply.github.com>
2020-07-08 12:30:39 -0700

    changed workflows.py

--- src/workflow.py
+++ src/workflow.py
@@ -5,7 +5,7 @@ import os
 from io import *
 
 class Calculation():
-    def __init__(self, work_dir, structure, k_dim, pseudo_list, start_setting = [], parallelization = [group, partition, num_nodes, time_out]):
+    def __init__(self, work_dir, structure, k_dim, pseudo_dir = '/global/common/sw/cray/cnl7/haswell/vasp/pseudopotentials/PBE/potpaw_PBE', pseudo_list = [     ], start_setting = [], parallelization = [group, partition, num_nodes, time_out]):
         """
         Calculation superclass defines and produces the sequence of input files and directories
         needed to compute a specified property in VASP. Input parameters, structure, pseudopotentials,
@@ -22,9 +22,9 @@ class Calculation():
         self._start_calc = start_setting
         self._structure = structure
         self._kgrid = k_dim
-        self._pseudo_dir = '/global/common/sw/cray/cnl7/haswell/vasp/pseudopotentials/PBE/potpaw_PBE'
+        self._pseudo_dir =
         self._pseudos = pseudolist
-        self._parallelization = ['knl','regular', 0,0, k_dim[0]*k_dim[1]*k_dim[2]] ##[computer, calc_type, nodes, nthreads, ppn]
+        self._parallelization = [jobname, machine, partition, nodes, ntasks, maxtime, ncore, kpar]
 
     def set_structure(self, structure):
         """Initializes structure for calculation"""
@@ -39,6 +39,9 @@ class Calculation():
         """Sets input parameters for ground state calculation"""
         assert calc_type == 'regular' or calc_type == 'colinear' or calc_type == 'noncolinear', 'Calcualtion type must be regular, colinear, or noncolinear'
         self._parallelization = ['knl', calc_type, nodes, nthreads, ppn]
+
+    def get_run_time(self):
+
 
 class RelaxationCalculation(Calculation):
     def __init__(self, relax_type, encut, nlec, nstep, work_dir, structure, k_dim, pseudo_list, epsilon=0.0001, nsteps=60, smear=0):
@@ -97,65 +100,100 @@ class SCFCalculation(Calculation):
         def make_calculation(self):
             """Sets input parameters for ground state calculation and makes files for calculation"""
             temp_dir = os.getcwd()
-            os.mkdir(self._workdir_)
+            os.mkdir(self._workdir)
             print("Work Directory now in:" + self._workdir_)
-            os.chdir(self._workdir_)
+            os.chdir(self._workdir)
             make_incar(self._input_)
             make_poscar(self._structure_)
             make_potcar(self._pseudos_)
             make_runscript(self._parallelization_)
             os.chdir(temp_dir)
 
-        def run(self):
-
         def get_total_energy(self):
+            energ_list = []
+            fl_nm = self._workdir + 'OUTCAR'
+            isfile = os.path.isfile(fl_nm)
+            if isfile == False:
+                print("OUTCAR file not present! try to re-run the calculation.")
+                pass
+            else:
+               with open(fl_nm, 'r') as f:
+                 for line in f.readlines():
+                   if 'TOTEN' in line:
+                     energ_list.append(line)
+               tot_energ = energ_list[len(energ_list)-1]
+            return float(tot_energ[30:40])
+
+         def get_run_time(self):
+            fl_nm = self._workdir + 'OUTCAR'
+            isfile = os.path.isfile(fl_nm)
+            if isfile == False:
+                print("OUTCAR file not present! try to re-run the calculation.")
+                pass
+            else:
+               with open(fl_nm, 'r') as f:
+                 for line in f.readlines():
+                   if 'Total CPU time used (sec):' in line:
+                       time_str = line
+           return float(time_str[49:57])
 
 
-        def get_forces(self):
+        def get_fermi(self):
+            fl_nm = self._workdir + 'OUTCAR'
+            isfile = os.path.isfile(fl_nm)
+            if isfile == False:
+                print("OUTCAR file not present! try to re-run the calculation.")
+                pass
+            else:
+               with open(fl_nm, 'r') as f:
+                 for line in f.readlines():
+                   if 'E-fermi :' in line:
+                       fermi_str = line
+           return float(fermi_str[12:18])
 
-        def get_charge_density(self):
+        #def get_mag_moment(self):
 
 
 
-class BandCalculation(Calculation):
-        def __init__(self, charge_option, wfn,  istart, work_dir, structure, k_dim, pseudo_lists, k_path = None, nbnds = None, lorbit = True , smear = False , sigma = 0.01, isym = 0):
-            """OUTPUT List:
-               Istart:
-                  - 0 = begin from scratch
-                  - 1 = continue job w energy cutoff
-                  - 2 = continue, restart w constant basis
-               ICHARG:
-                   -   0 (computes from initial wfn)
-                   - = 1 extrapolate from old positions, reads from CHCAR
-
-              LORBIT =  10 ==> not decomposed DOS
-              LORBIT = 11 ==> decomposed DOS
-            """
-            super(SCFCalculation, self).__init__(self, work_dir, structure, k_dim, pseudo_lists)
-
-            self._bands_options_ = [k_path, nbnds, charge_option]
-
-            self._scf_setting_ = [charge_option, prec, encut, nstep, epsilon, pseudo, n_elect.structure, smear, sigma, isym]
-            self._ionic_  = []
-            self._dos_ = [lorbit, dos_pts]
-            self._bands_options_ = [k_path, nbnds, charge_option]
-            self._magentic_options_ = [spin_polar, magmom, spin_orbit]
-            self._hubbard_ = [dft_u, dudarev, ldaul, u_param, j_param, lda_mix]
-            self._hybrid_ = [hf_calc, hf_fft, hf_max, hf_scren, hf_xch]
-            self._rho_decomp_ = [par_chg, en_rng, ref_ef, over_k, over_bnd]
-
-        def make_calculation(self):
-                """Sets input parameters for ground state calculation and makes files for calculation"""
-            temp_dir = os.getcwd()
-            os.mkdir(self._workdir)
-            print("Work Directory now in:" + self._workdir)
-            os.chdir(self._workdir)
-            make_incar(self._pseudos, self._input)
-            make_poscar(self. )
-            make_potcar(self._pseudos)
-            make_runscript(self._parallelization)
-            os.chdir(temp_dir)
-
+# class BandCalculation(Calculation):
+#         def __init__(self, charge_option, wfn,  istart, work_dir, structure, k_dim, pseudo_lists, k_path = None, nbnds = None, lorbit = True , smear = False , sigma = 0.01, isym = 0):
+#             """OUTPUT List:
+#                Istart:
+#                   - 0 = begin from scratch
+#                   - 1 = continue job w energy cutoff
+#                   - 2 = continue, restart w constant basis
+#                ICHARG:
+#                    -   0 (computes from initial wfn)
+#                    - = 1 extrapolate from old positions, reads from CHCAR
+#
+#               LORBIT =  10 ==> not decomposed DOS
+#               LORBIT = 11 ==> decomposed DOS
+#             """
+#             super(SCFCalculation, self).__init__(self, work_dir, structure, k_dim, pseudo_lists)
+#
+#             self._bands_options_ = [k_path, nbnds, charge_option]
+#
+#             self._scf_setting_ = [charge_option, prec, encut, nstep, epsilon, pseudo, n_elect.structure, smear, sigma, isym]
+#             self._ionic_  = []
+#             self._dos_ = [lorbit, dos_pts]
+#             self._bands_options_ = [k_path, nbnds, charge_option]
+#             self._magentic_options_ = [spin_polar, magmom, spin_orbit]
+#             self._hubbard_ = [dft_u, dudarev, ldaul, u_param, j_param, lda_mix]
+#             self._hybrid_ = [hf_calc, hf_fft, hf_max, hf_scren, hf_xch]
+#             self._rho_decomp_ = [par_chg, en_rng, ref_ef, over_k, over_bnd]
+#
+#         def make_calculation(self):
+#                 """Sets input parameters for ground state calculation and makes files for calculation"""
+#             temp_dir = os.getcwd()
+#             os.mkdir(self._workdir)
+#             print("Work Directory now in:" + self._workdir)
+#             os.chdir(self._workdir)
+#             make_incar(self._pseudos, self._input)
+#             make_poscar(self. )
+#             make_potcar(self._pseudos)
+#             make_runscript(self._parallelization)
+#             os.chdir(temp_dir)
+#
 
 
 #class PhononCalculation(Calculation):
@@ -174,25 +212,38 @@ class BandCalculation(Calculation):
 #class EFieldCalculation(Calculation):
 
 
-
-
 class SerialComputeFlow():
 """
 Sets up a serial set of compuations which iterate over a desired degree of freedom
 (i.e. strain, dopant type/position, substrate)
 """
-    def __init__(self, input):
+    def __init__(self, workdir, paramter,   ):
     """
     Sets up a serial set of compuations which iterate over a desired degree of freedom
     (i.e. strain, dopant type/position, substrate)
     """
-        self._algorithm =
-        self._bands =
-        self._rho   =
-        self._spin_orbit =
-        self._charge_compensate =
-        self._spin_polar  =
-        self._dos =
+        self._workdir = workdir
+        self._parameter = paramter
+        self._dir_prefix = paramter
+        self._dir_names = []
+        self._ncalc = 0
+        self._param_list =
+        self._calc_list = []
+        self._runscript =
+
+    def setup_calc_series(self):
+        os.chdir(self._workdir)
+        param_list = self._param_list
+        for i in range(self._ncalc):
+            dir_name = self._dir_prefix + param_list[i]
+            self._dir_names.append(dirname)
+            os.mkdir(dirname)
+
+    def add_calc(self):
+
+    def remove_calc(self):
+
+    def run_status(self):
 
 
 
@@ -244,13 +295,21 @@ class MagenticAnisotropyFlow(SerialComputeFlow):
     Sets up a serial set of compuations which iterate over a desired degree of freedom
     (i.e. strain, dopant type/position, substrate)
     """
-    def __init__(self):
-        super(RelaxationWorkflow, self).__init__(inialized inputs, reference_orient)
-        self._reference_direction =
-
-
-
-        self._azimuth_range =
-        self._polar_range =
-        self._symmetryreduce =
-        self._magmoments =
+    def __init__(self, cl_calc, polar_range, azimuth_range, ref_orient):
+        self._reference_orientation = ref_orient
+        self._cl_calc  =  cl_calc
+        self._collinear_calc = cl_calc
+        self._azimuth_range = polar_range
+        self._polar_range = azimuth_range,
+        self._symmetryreduce = [False, None, None]
+        def generate_spin_axes_h(self):
+            spin_list = []
+            itr = 0
+            for i in self._azimuth_range:
+                for j in  self._polar_range:
+                    spin_list.append([itr, np.sin(i)*np.cos(j), np.sin(i)*np.sin(j), cos(j)])
+                    itr += 1
+            return spin_list
+        self._spin_list = self.generate_spin_axes_h()
+        super(SerialComputeFlow, self).__init__(inialized inputs, reference_orient)
+           
